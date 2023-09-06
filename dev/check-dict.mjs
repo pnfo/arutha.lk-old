4c6611a10a33ccf81492fe0e1d2025df3e093e9a
pathnirvana <pathnirvana@gmail.com>
2023-09-06 20:22:05 +0700

    combine sankshipta dicts

--- dev/check-dict.mjs
+++ dev/check-dict.mjs
@@ -6,21 +6,22 @@ import fs from 'fs'
 import vkb from 'vkbeautify'
 
 const dictId = 'sankshiptha'
-const sanketha = JSON.parse(fs.readFileSync(`public/${dictId}-sanketha.json`))
+const sanketha = JSON.parse(fs.readFileSync(`public/sankshiptha-sanketha.json`))
 Object.keys(sanketha).forEach(san => sanketha[san] = {title: sanketha[san], count: 0})
 const entries = fs.readFileSync(`public/${dictId}-dict.txt`, 'utf-8').split('\n\n')
     .map(g => g.split('\n').map(l => l.trim())).map(g => ({word: g[0], meaning: g.slice(1)}))
 
 console.log(`found ${entries.length} entries`)
+const notFound = []
 
 const json = entries.forEach(({word, meaning}) => {
-    if (word.includes('[') || word.includes(' (')) console.log(`brackets in word ${word}`)
+    if (/[^, \-\u200d\u0D80-\u0DFF]/.test(word)) notFound.push(word)//console.log(`brackets in word ${word}`)
     const subWords = word.split(', ')
     if (subWords.length != word.split(',').length) console.log(`does not have space after comma in ${word}`)
-    if (subWords.length > 1 && subWords.filter(w => w.length <= 2).length) console.log(`short word part in ${word}`)
+    //if (subWords.length > 1 && subWords.filter(w => w.length <= 2).length) console.log(`short word part in ${word}`)
     if (!meaning.length) console.log(`empty meaning for word ${word}`)
-    const shortMeaning = meaning.filter(l => l.length < 10 && !l.includes('['))
-    if (shortMeaning.length) console.log(`short line in meaning of word ${word}`)
+    //const shortMeaning = meaning.filter(l => l.length < 5 && !l.includes('['))
+    //if (shortMeaning.length) console.log(`short line in meaning of word ${word}`)
     if (meaning.length > 20) console.log(`meaning too many lines in word ${word}, ${meaning.length}`)
     if (meaning.findIndex(l => l.includes('+')) > 0) console.log(`+ found on a later line ${word}`)
     
@@ -39,7 +40,7 @@ const json = entries.forEach(({word, meaning}) => {
     
     return [word, meaning]
 })
-
+console.log(notFound.map(n => n.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|'))
 fs.writeFileSync(`public/${dictId}-sanketha-counts.json`, vkb.json(JSON.stringify(sanketha)), 'utf-8')
 //Object.entries(sankethaCounts)
 //    .sort((a, b) => a[0].localeCompare(b[0])).map(pair => pair.join(',')).join('\n'), 'utf-8')
